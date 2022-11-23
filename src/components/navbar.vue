9e57862937769d5f336546b76d319eecff2945d2
Nwabuwa Philip <philipnwabuwa@gmail.com>
2022-11-23 04:25:30 +0100

    added tailwind

---
+++ src/components/navbar.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<template>
+  <nav>
+    <div>
+      <h1 class="text-blue-800 underline p-3 font-bold text-2xl uppercase">
+        My App
+      </h1>
+    </div>
+  </nav>
+</template>
